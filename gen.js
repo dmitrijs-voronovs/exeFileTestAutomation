40e7a565c1d88e581c89dadeee70c8b9a083c018
dmitrijs-voronovs <dmitrijs.voronovs@scandiweb.com>
2020-05-02 22:22:23 +0300

    more tests

--- gen.js
+++ gen.js
@@ -10,12 +10,36 @@ var fs = require('fs');
 const GEN_FILE = './gen.txt';
 fs.truncateSync(GEN_FILE);
 
-function appendF(data) {
-    fs.appendFileSync(GEN_FILE, data);
-}
-
-for (let hundreds = 0; hundreds < 1000; hundreds += 100) {
-    for (let i = hundreds + 2; i < hundreds + 100 + 1; i++) {
-        appendF(`${i - 1} ${ i}\n`);
+function wr(...args) {
+    if (args[1] || args[1] === 0) {
+        fs.appendFileSync(GEN_FILE, args[0] + ' ' + args[1] + '\n');
+    } else {
+        fs.appendFileSync(GEN_FILE, args[0] + '\n');
     }
 }
+
+// ========================================
+
+// for (let hundreds = 0; hundreds < 1000; hundreds += 100) {
+//     for (let i = hundreds + 2; i < hundreds + 100 + 1; i++) {
+//         wr(`${i - 1} ${ i}\n`);
+//     }
+// }
+
+wr(110);
+
+const allUpps = [];
+
+for (let i = 0; i < 10; i++) {
+    allUpps.push(i * 10 + 1);
+    for (let j = 2; j <= 10; j++) {
+        wr(i * 10 + 1, i * 10 + j);
+    }
+}
+
+for (let i = 0; i < allUpps.length - 1; i++) {
+    wr(allUpps[i], allUpps[i + 1]);
+}
+
+wr(0, 0)
+console.log(allUpps);
